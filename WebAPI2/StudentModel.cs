a3dc54d3974ce1ccf22a8026fa4a713920afa67e
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-09 19:49:14 +1200

    test 2

---
+++ WebAPI2/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI2
+{
+    public class StudentModel
+    {
+        public string StudentName { get; set; }
+        public string UniversityName { get; set; }
+    }
+}
--- Week4/Controllers/HomeController.cs
+++ Week4/Controllers/HomeController.cs
@@ -33,6 +33,15 @@ namespace Week4.Controllers
             return View(nameof(Privacy));
         }
 
+        [HttpGet("Students"), ProducesResponseType(typeof(StudentModel), 200)]
+        public List<StudentModel> GetStudents()
+        {
+            var students = new List<StudentModel>();
+            students.Add(new StudentModel { StudentName = "Student1", UniversityName = "University1" });
+            students.Add(new StudentModel { StudentName = "Student2", UniversityName = "University2" });
+            return students;
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
